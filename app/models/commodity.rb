b3bbc0edc99de4a503fc2f6508e4f7eaee4895ea
iwasaki_d <d.iwasaki.g@gmail.com>
2016-09-28 17:15:02 +0900

    ポジションが少ないほど上位に来るように修正

--- app/models/commodity.rb
+++ app/models/commodity.rb
@@ -1,4 +1,4 @@
 class Commodity < ActiveRecord::Base
 
-  scope :home_list, -> { where(view_enabled: 1).order('position desc') }
+  scope :home_list, -> { where(view_enabled: 1).order(:position) }
 end
